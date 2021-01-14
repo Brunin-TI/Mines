f48baf56dce3dc7175575677798a63a265da374f
Brunin-TI <brunotorresaraujo@hotmail.com>
2021-01-13 22:18:58 -0300

    flag1

--- App.js
+++ App.js
@@ -11,6 +11,17 @@ export default class App extends Component {
         <Text>Tamanho da grade:
         {params.getColumnsAmount()}x{params.getColumnsAmount()}</Text>
         <Field />
+        <Field opened />
+        <Field opened nearMines={1}/>
+        <Field opened nearMines={2}/>
+        <Field opened nearMines={3}/>
+        <Field opened nearMines={6}/>
+        <Field mined />
+        <Field opened mined />
+        <Field opened mined exploded />
+        <Field flagged />
+        <Field flagged opened />
+        
       </View>
     );
   }
